cd34b0c04d747031158d9bd98242de2795fe86a8
User Name <jessnsq.codes@gmail.com>
2023-10-20 19:59:26 -0400

    adding budgets functionality

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,23 +1,50 @@
 import { Container, Stack, Button} from 'react-bootstrap';
 import './App.css';
 import BudgetCard from './components/BudgetCard';
+import AddBudget from './components/AddBudget';
+import {React, useState} from 'react';
+import { useBudgets } from './contexts/BudgetsContext';
+
 
 function App() {
-  return <Container className='my-4'> 
+  // We'll use React.useState to keep track of whether the modal is open or not, by default it's false/closed
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  // We'll use the useBudgets hook to get the budgets from the BudgetsContext
+  const {budgets, getBudgetsExpenses} = useBudgets();
+
+  return(
+  <>
+  <Container className='my-4'> 
     <Stack direction='horizontal' gap='2' className='mb-4'>
       <h1 className='me-auto'> Budgets </h1>  
-      <Button variant='primary'> Add Buget </Button>
+      {/* onClick function will set the state of showAddBudgetModal to true, which will open the modal, so that whenever we click on the show modal we'll see the pop-up to add a new budget */}
+      <Button variant='primary' onClick={()=> setShowAddBudgetModal(true)}> Add Buget </Button>
       <Button variant='outline-primary'> Add Expense </Button>
     </Stack>
+
+
     <div id = "grid-container">
-      <BudgetCard 
-        name='Entertainment' 
-        gray
-        amount={200} 
-        max={800}> 
-      </BudgetCard>
+      {/* TO-DO: Create a separate component for the BudgetCard */}
+      {/* we'll loop through the budgets array and render a BudgetCard for each budget */}
+      {budgets.map(budget => {
+        //this function will take all the expenses, add all the amounts together, and return the total amount of expenses for a specific budget/category
+        const amount = getBudgetsExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
+
+        return(
+        <BudgetCard 
+          name={budget.name} 
+          amount={0} 
+          max={budget.max}
+        /> 
+        )
+      })
+        };
+      
     </div>
   </Container>
+  <AddBudget show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)}/>
+  </>
+  )
 }
 
 export default App;
---
+++ budget-app/src/components/AddBudget.js
@@ -0,0 +1,51 @@
+import {React, useRef} from 'react';
+import { Form, Modal, Button } from 'react-bootstrap';
+import { useBudgets } from '../contexts/BudgetsContext';
+
+
+
+export default function AddBudget({  show, handleClose }) {
+    //nameRef is a reference to the input field
+    const nameRef = useRef();
+    const maxRef = useRef();
+    const { addBudget } = useBudgets();
+    const handleSubmit = (e) => {
+        // Prevents page from refreshing
+        e.preventDefault();
+
+        // Add budget to the list by calling the function 'addBudget' from useBudgets and passing in the name and max as parameters
+        addBudget({
+                name: nameRef.current.value,
+                //use parseFloat to convert the string to a number
+                max: parseFloat(maxRef.current.value),
+            })
+        // whenever we submit, we'll close the modal
+            handleClose();
+    };
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Form onSubmit={handleSubmit}>
+                <Modal.Header closeButton>
+                    <Modal.Title> Add Budget </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {/* We'll use Form to add budget */}
+                    <Form.Group className = 'mb-3' controlId='name'>
+                        <Form.Label> Category </Form.Label>
+                        {/* User's input */}
+                        <Form.Control ref= {nameRef} type='text' placeholder='Enter category' required />
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='max-spending'>
+                        <Form.Label> Max Spending </Form.Label>
+                        <Form.Control ref= {maxRef} type='number' placeholder='Enter max' requiredmin={0} step={.01}/>
+                    </Form.Group>
+                    <div className='d-flex justify-content-end'>
+                        <Button variant='primary' type='submit'> Add Budget </Button>
+                        <Button variant='secondary' onClick={handleClose}> Cancel </Button>
+                    </div>
+                </Modal.Body>
+            </Form>
+        </Modal>
+  )
+};
